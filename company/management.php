2c8a41ffbe0c360db0b885d7c286aad1ebdab477
Оксана Н. Белова <oksana.belova@a1.dom>
2017-09-19 12:59:56 +0300

    получены значения из get list сотрудников и отделов

--- company/management.php
+++ company/management.php
@@ -4,47 +4,37 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Наша команда");
 ?>
 <?$APPLICATION->IncludeComponent(
-	"bitrix:news",
-	"sotrudniki",
+	"my_component:news.list",
+	"sotrudniki_1",
 	Array(
-		"ADD_ELEMENT_CHAIN" => "N",
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
 		"ADD_SECTIONS_CHAIN" => "Y",
 		"AJAX_MODE" => "N",
 		"AJAX_OPTION_ADDITIONAL" => "",
 		"AJAX_OPTION_HISTORY" => "N",
 		"AJAX_OPTION_JUMP" => "N",
 		"AJAX_OPTION_STYLE" => "Y",
-		"BROWSER_TITLE" => "-",
 		"CACHE_FILTER" => "N",
 		"CACHE_GROUPS" => "Y",
 		"CACHE_TIME" => "36000000",
 		"CACHE_TYPE" => "A",
 		"CHECK_DATES" => "Y",
-		"DETAIL_ACTIVE_DATE_FORMAT" => "d.m.Y",
-		"DETAIL_DISPLAY_BOTTOM_PAGER" => "Y",
-		"DETAIL_DISPLAY_TOP_PAGER" => "N",
-		"DETAIL_FIELD_CODE" => array("",""),
-		"DETAIL_PAGER_SHOW_ALL" => "Y",
-		"DETAIL_PAGER_TEMPLATE" => "",
-		"DETAIL_PAGER_TITLE" => "Страница",
-		"DETAIL_PROPERTY_CODE" => array("",""),
-		"DETAIL_SET_CANONICAL_URL" => "N",
+		"COMPONENT_TEMPLATE" => ".default",
+		"DETAIL_URL" => "",
 		"DISPLAY_BOTTOM_PAGER" => "Y",
 		"DISPLAY_DATE" => "Y",
 		"DISPLAY_NAME" => "Y",
 		"DISPLAY_PICTURE" => "Y",
 		"DISPLAY_PREVIEW_TEXT" => "Y",
 		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => array(0=>"",1=>"",),
+		"FILTER_NAME" => "",
 		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
 		"IBLOCK_ID" => "7",
 		"IBLOCK_TYPE" => "employees",
 		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
-		"LIST_ACTIVE_DATE_FORMAT" => "d.m.Y",
-		"LIST_FIELD_CODE" => array("",""),
-		"LIST_PROPERTY_CODE" => array("",""),
+		"INCLUDE_SUBSECTIONS" => "Y",
 		"MESSAGE_404" => "",
-		"META_DESCRIPTION" => "-",
-		"META_KEYWORDS" => "-",
 		"NEWS_COUNT" => "20",
 		"PAGER_BASE_LINK_ENABLE" => "N",
 		"PAGER_DESC_NUMBERING" => "N",
@@ -53,9 +43,14 @@ $APPLICATION->SetTitle("Наша команда");
 		"PAGER_SHOW_ALWAYS" => "N",
 		"PAGER_TEMPLATE" => ".default",
 		"PAGER_TITLE" => "Новости",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
 		"PREVIEW_TRUNCATE_LEN" => "",
-		"SEF_MODE" => "N",
+		"PROPERTY_CODE" => array(0=>"",1=>"",),
+		"SET_BROWSER_TITLE" => "Y",
 		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "Y",
+		"SET_META_KEYWORDS" => "Y",
 		"SET_STATUS_404" => "N",
 		"SET_TITLE" => "Y",
 		"SHOW_404" => "N",
@@ -63,17 +58,83 @@ $APPLICATION->SetTitle("Наша команда");
 		"SORT_BY2" => "SORT",
 		"SORT_ORDER1" => "DESC",
 		"SORT_ORDER2" => "ASC",
-		"STRICT_SECTION_CHECK" => "N",
-		"USE_CATEGORIES" => "N",
-		"USE_FILTER" => "N",
-		"USE_PERMISSIONS" => "N",
-		"USE_RATING" => "N",
-		"USE_RSS" => "N",
-		"USE_SEARCH" => "N",
-		"USE_SHARE" => "N",
-		"VARIABLE_ALIASES" => Array("ELEMENT_ID"=>"ELEMENT_ID","SECTION_ID"=>"SECTION_ID")
+		"STRICT_SECTION_CHECK" => "N"
 	)
 );?>
+<?
+   if(CModule::IncludeModule("iblock"))
+   {
+       //ПОДКЛЮЧАЕМ МОДУЛЬ ИНФОБЛОКИ
+
+       $arSelect = Array("ID", "NAME","PROPERTY_*","DETAIL_TEXT","PREVIEW_PICTURE");
+       $arFilter = Array("IBLOCK_ID"=>7, "ACTIVE_DATE"=>"Y", "ACTIVE"=>"Y");
+
+       $res = CIBlockElement::GetList(
+           Array("PROPERTY_20" => "ASC"),
+           $arFilter,
+           false,
+           Array("nPageSize"=>50),
+           $arSelect);
+
+        while($ob = $res->GetNextElement()){
+            $arItem_sotr = $ob->GetFields();
+            $arItem_sotr["PROPERTIES"] = $ob->GetProperties();
+            $arResult_employees[] = $arItem_sotr;
+            }
+    }
+    if(CModule::IncludeModule("iblock"))
+    {
+        //ПОДКЛЮЧАЕМ МОДУЛЬ ИНФОБЛОКИ
+
+        $arSelect = Array("ID","NAME","PROPERTY_*",);
+        $arFilter = Array("IBLOCK_ID"=>9, "ACTIVE_DATE"=>"Y", "ACTIVE"=>"Y");
+
+        $res = CIBlockElement::GetList(
+            Array(),
+            $arFilter,
+            false,
+            Array("nPageSize"=>50),
+            $arSelect);
+
+         while($ob = $res->GetNextElement()){
+             $arItem_department = $ob->GetFields();
+             $arItem_department["PROPERTIES"] = $ob->GetProperties();
+             $arResult_department[] =  $arItem_department;
+             }
+     }
+?>
+<?
+foreach($arResult_department as $department)
+{
+    foreach($arResult_employees as $employee)
+    {
+        if ($department["ID"]==$employee["PROPERTY_20"])
+        {
+            $department["EMPLOYEES"][] = $employee;
+        }
+    }
+    $arResult[] = $department;
+}
+?>
 <div class="container">
+<?foreach($arResult_department as $department):?>
+        <h3><?echo $department["NAME"]?></h3> <br>
+        <?foreach($arResult_employees as $employee):?>
+            <?if ($employee["PROPERTY_20"]==$department["ID"]):?>
+                <div style="margin-top:10px">
+                <b><?echo $employee["NAME"]?></b> <br>
+                <?$ImagePath = CFile::GetPath( $employee["PREVIEW_PICTURE"]);?>
+                <img
+                    class="preview_picture"
+                    src="<?=$ImagePath?>"
+                    width="115"
+                    height="150"
+                    style="float:left, margin:10px"
+                    /><br>
+                <?echo $employee ["~DETAIL_TEXT"]?> <br>
+                </div>
+            <?endif?>
+        <?endforeach;?>
+<?endforeach;?>
 </div>
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- local/templates/triangle/components/bitrix/news/sotrudniki/bitrix/news.list/.default/template.php
+++ local/templates/triangle/components/bitrix/news/sotrudniki/bitrix/news.list/.default/template.php
@@ -11,45 +11,25 @@
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
 $this->setFrameMode(true);
+// pre($arResult["ITEMS"]);
+// foreach ($arResult["ITEMS"] as $item) {
+// 	echo $item['ID'], '<br>';
+// 	$items[$item['ID']] = $item;
+//
+// }
+pre($arResult["ITEMS"]);
+
 ?>
+<h3>Подразделения компании</h3>
+
 <div class="news-list">
-<?if($arParams["DISPLAY_TOP_PAGER"]):?>
-	<?=$arResult["NAV_STRING"]?><br />
-<?endif;?>
 <?foreach($arResult["ITEMS"] as $arItem):?>
 	<?
 	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
 	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
 	?>
 	<p class="news-item" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
-		<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
-			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
-				<a href="<?=$arItem["DETAIL_PAGE_URL"]?>"><img
-						class="preview_picture"
-						border="0"
-						src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
-						width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
-						height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
-						alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
-						title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
-						style="float:left"
-						/></a>
-			<?else:?>
-				<img
-					class="preview_picture"
-					border="0"
-					src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>"
-					width="<?=$arItem["PREVIEW_PICTURE"]["WIDTH"]?>"
-					height="<?=$arItem["PREVIEW_PICTURE"]["HEIGHT"]?>"
-					alt="<?=$arItem["PREVIEW_PICTURE"]["ALT"]?>"
-					title="<?=$arItem["PREVIEW_PICTURE"]["TITLE"]?>"
-					style="float:left"
-					/>
-			<?endif;?>
-		<?endif?>
-		<?if($arParams["DISPLAY_DATE"]!="N" && $arItem["DISPLAY_ACTIVE_FROM"]):?>
-			<span class="news-date-time"><?echo $arItem["DISPLAY_ACTIVE_FROM"]?></span>
-		<?endif?>
+
 		<?if($arParams["DISPLAY_NAME"]!="N" && $arItem["NAME"]):?>
 			<?if(!$arParams["HIDE_LINK_WHEN_NO_DETAIL"] || ($arItem["DETAIL_TEXT"] && $arResult["USER_HAVE_ACCESS"])):?>
 				<a href="<?echo $arItem["DETAIL_PAGE_URL"]?>"><b><?echo $arItem["NAME"]?></b></a><br />
@@ -57,30 +37,41 @@ $this->setFrameMode(true);
 				<b><?echo $arItem["NAME"]?></b><br />
 			<?endif;?>
 		<?endif;?>
-		<?if($arParams["DISPLAY_PREVIEW_TEXT"]!="N" && $arItem["PREVIEW_TEXT"]):?>
-			<?echo $arItem["PREVIEW_TEXT"];?>
-		<?endif;?>
-		<?if($arParams["DISPLAY_PICTURE"]!="N" && is_array($arItem["PREVIEW_PICTURE"])):?>
-			<div style="clear:both"></div>
-		<?endif?>
-		<?foreach($arItem["FIELDS"] as $code=>$value):?>
-			<small>
-			<?=GetMessage("IBLOCK_FIELD_".$code)?>:&nbsp;<?=$value;?>
-			</small><br />
-		<?endforeach;?>
+
 		<?foreach($arItem["DISPLAY_PROPERTIES"] as $pid=>$arProperty):?>
-			<small>
-			<?=$arProperty["NAME"]?>:&nbsp;
 			<?if(is_array($arProperty["DISPLAY_VALUE"])):?>
-				<?=implode("&nbsp;/&nbsp;", $arProperty["DISPLAY_VALUE"]);?>
+				<?foreach($arProperty["VALUE"] as $x):?>
+					<a onClick="show()"><?=$arProperty[LINK_ELEMENT_VALUE][$x][NAME];?></a>
+					<br>
+					<div id="win" style="display:none;">
+					   <div class="overlay"></div>
+					      <div class="visible">
+					        <h3><? echo $arProperty[LINK_ELEMENT_VALUE][$x][NAME];?></h3>
+					          <div class="content">
+					            <p>Содержание</p>
+					          </div>
+							  <button type="button" onClick="getElementById('win').style.display='none';">Закрыть</button>
+					    </div>
+					</div>
+				<?endforeach;?>
 			<?else:?>
-				<?=$arProperty["DISPLAY_VALUE"];?>
-			<?endif?>
-			</small><br />
+			<?$x=($arProperty["VALUE"][0])?>
+			<?$text=$arProperty[LINK_ELEMENT_VALUE][$x][PROPERTY_WORKER_DETAIL_TEXT];?>
+			<a onClick="getElementById('win').removeAttribute('style');" type="button"><?=$arProperty[LINK_ELEMENT_VALUE][$x][NAME];?></a>
+			<div id="win" style="display:none;">
+			   <div class="overlay"></div>
+			      <div class="visible">
+			        <h3>Заголовок окна</h3>
+			          <div class="content">
+			            <p>Содержание</p>
+			          </div>
+					  <button type="button" onClick="getElementById('win').style.display='none';">Закрыть</button>
+			    </div>
+			</div>
+			<?endif?><br/>
 		<?endforeach;?>
 	</p>
 <?endforeach;?>
-<?if($arParams["DISPLAY_BOTTOM_PAGER"]):?>
-	<br /><?=$arResult["NAV_STRING"]?>
-<?endif;?>
+<script>
+getElementById('win').removeAttribute('style');
 </div>
